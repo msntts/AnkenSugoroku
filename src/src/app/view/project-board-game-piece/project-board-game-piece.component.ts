a5d7ec834fcc27173e950ccb2a7bb71a78c54328
Makoto Katsukura <thinkingobo@gmail.com>
2021-04-10 15:36:39 +0900

    　* 移動時のアニメーション機能と、ドラッグ中の移動元の表示機能を追加    - 移動時のアニメーション機能を無効化したい場合は、moveAnimationEnable を false に設定してください    - ドラッグ中の移動元の表示機能を無効化したい場合は、showFromMark　を false　に設定してください。  * マスに影を付けました
    以上

--- src/src/app/view/project-board-game-piece/project-board-game-piece.component.ts
+++ src/src/app/view/project-board-game-piece/project-board-game-piece.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BoardDataService } from 'src/app/controller/board-data.service';
-import { RecordService} from 'src/app/controller/record.service';
+import { RecordService } from 'src/app/controller/record.service';
 
 @Component({
   selector: 'app-project-board-game-piece',
@@ -19,21 +19,37 @@ export class ProjectBoardGamePieceComponent implements OnInit {
   /* フィルタ */
   public filter: string = "";
 
+  /* 駒の透明度 */
+  public opacity: number = 1.0;
+
+  /* 移動元の表示 */
+  private fromMarkOn: boolean = false;
+
+
   /** 駒のマス内判定点までのオフセット（左上から） X方向 */
   private readonly c_offset_x: number = 50;
+  
   /** 駒のマス内判定点までのオフセット（左上から） Y方向 */
   private readonly c_offset_y: number = 50;
-  
+
+  /**  移動アニメーション 有効/無効 */
+  private readonly moveAnimationEnable = true;
+
+  /** ドラッグ中に移動元を明示する  有効/無効 */
+  private readonly showFromMark = true;
+
+
   /**
    * コンストラクタ
    * @param boardDataService 盤情報サービス
    */
-   constructor(
+  constructor(
     private boardDataService: BoardDataService,
     private recordService: RecordService,
   ) {
     this.boardDataService.observable.subscribe(() => {
-      this.OnInit()}
+      this.OnInit()
+    }
     );
   }
 
@@ -48,12 +64,28 @@ export class ProjectBoardGamePieceComponent implements OnInit {
     if (square == null) {
       square = this.boardDataService.findSquare(1)
     }
-    
+
     if (square != null) {
       this.x = `${square.X}px`;
       this.y = `${square.Y}px`;
     }
   }
+
+  /**
+   * ドラッグ開始時のイベント
+   * @param event 
+   */
+  onDragStarted(event: any) {
+
+    // ドラッグ中に移動元を表示
+    if (this.showFromMark) {
+      // 移動元表示マークON
+      this.fromMarkOn = true;
+      // ドラッグ中は半透明にする
+      this.opacity = 0.5;
+    }
+  }
+
 
   /**
    * ドラッグ完了時のイベント
@@ -70,13 +102,35 @@ export class ProjectBoardGamePieceComponent implements OnInit {
 
     // マスにいるかどうか
     // ※マスの中にいるかどうかの判定時、駒の左上座標を用いるとユーザーの操作感と不一致が生じる。
-	// (駒の左上がマス内に入っていないと、	「マス内に入っていない」と判定されてしまう)
+    // (駒の左上がマス内に入っていないと、	「マス内に入っていない」と判定されてしまう)
     //   このため、マスの中にいるかどうかの判定に用いる点は、左上座標からオフセット分画像の内側に入った位置を用いる
     const square = this.boardDataService.findSquareByPosition(newX + this.c_offset_x, newY + this.c_offset_y);
 
     if (square != null) {
       // 現在の状態から移動できるマスかどうか
       if (this.boardDataService.isMovable(this.id, square.Id)) {
+
+        // ドラッグ中に移動元を表示する場合
+        if (this.showFromMark) {
+          // ドラッグ終了
+          this.fromMarkOn = false;
+          this.opacity = 1.0;
+        }
+
+        // 移動アニメーションをする場合
+        if (this.moveAnimationEnable) {
+          // 移動する
+          let tox: number = parseInt(this.x);
+          let toy: number = parseInt(this.y);
+          let fromx: number = tox - x;
+          let fromy: number = toy - y;
+
+          for (let i = 0; i < 100; i++) {
+            this.x = `${fromx + (tox - fromx) * i / 100}px`;
+            this.y = `${fromy + (toy - fromy) * i / 100}px`;
+            await this.sleep(10)
+          }
+        }
         // 移動する
         this.x = `${square.X}px`;
         this.y = `${square.Y}px`;
@@ -86,27 +140,37 @@ export class ProjectBoardGamePieceComponent implements OnInit {
 
         // id更新
         this.id = square.Id;
-        
+
         // 最新のidを更新
         this.recordService.setLatestSquareId(this.id);
 
-      }else{
+      } else {
         // 移動できない場合
 
         // 色を反転させるアニメーションを表示
         await this.sleep(100);
-        this.filter="invert(100%)";
+        this.filter = "invert(100%)";
         await this.sleep(100);
-        this.filter="invert(0%)";
+        this.filter = "invert(0%)";
         await this.sleep(100);
-        this.filter="invert(100%)";
+        this.filter = "invert(100%)";
         await this.sleep(100);
-        this.filter="invert(0%)";
+        this.filter = "invert(0%)";
         await this.sleep(100);
-        this.filter="invert(100%)";
+        this.filter = "invert(100%)";
         await this.sleep(100);
-        this.filter="";
+        this.filter = "";
+
+        // ドラッグ中に移動元を表示する場合
+        if (this.showFromMark) {
+          // ドラッグ終了
+          this.fromMarkOn = false;
+          this.opacity = 1.0;
+        }
+
       }
+
+
     }
 
     // ドラッグの状態は元に戻す
@@ -114,15 +178,15 @@ export class ProjectBoardGamePieceComponent implements OnInit {
     event.source._dragRef.reset();
   }
 
-    /**
-   * 指定された時間、処理を中断する
-   * 本処理を呼び出す場合は、awaitをつけてください。
-   * @param milliseconds ミリ秒
-   */
-     private sleep(milliseconds: number) {
-      return new Promise<void>(resolve => {
-        setTimeout(() => resolve(), milliseconds);
-      });
-    }
-  
+  /**
+ * 指定された時間、処理を中断する
+ * 本処理を呼び出す場合は、awaitをつけてください。
+ * @param milliseconds ミリ秒
+ */
+  private sleep(milliseconds: number) {
+    return new Promise<void>(resolve => {
+      setTimeout(() => resolve(), milliseconds);
+    });
+  }
+
 }
