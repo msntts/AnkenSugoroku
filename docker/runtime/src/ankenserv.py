25db4a71cd64897121dff04c0add7362e66af6ea
msntts <terzonome-github@yahoo.co.jp>
2021-04-24 18:21:20 +0900

    rest apiを受け付けるサーバのインフラ構築(処理はまだない) - http://<host ip>:8080/sugoroku   - 案件すごろくのリソースをダウンロード   - http://<host.ip>:8080/api/     - rest api(anken_server.py)

---
+++ docker/runtime/src/ankenserv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return name
+
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
+
+## おまじない
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
